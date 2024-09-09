322968ce1dd3f6cb7b0986395eae1116f5aa7e14
keal khondekar <kewalkhondekar25@gmail.com>
2024-09-09 17:37:01 +0530

    feat: hello world added

---
+++ Begin/helloworld.cpp
@@ -0,0 +1,14 @@
+//pre-processor-directives - inclusive
+#include <iostream>
+
+using namespace std;
+// using std::cout;
+// using std::endl;
+
+int main(){
+  // std::cout << "Hello World" << std::endl;
+  cout << "Hello World" << endl;
+  return 0;
+}
+
+
