53d18dc1ae14f0d08a4282dcd3cd797b7070264d
keal khondekar <kewalkhondekar25@gmail.com>
2024-09-11 23:28:39 +0530

    feat: arithmatic operators added

--- Operators/main.cpp
+++ Operators/main.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main(){
-  cout << "operators";
+  cout << "Operators" << endl;
+  
+  cout << "Arithmatic Operators";
+  int a = 10, b = 5;
+  int sum = a + b;
+  int sub = a - b;
+  int mul = a * b;
+  int div = a/b;
+  cout << "sum: " << sum << endl << "sub: " << sub << endl << "mul: " << mul << endl << "div: " << div;
 };
