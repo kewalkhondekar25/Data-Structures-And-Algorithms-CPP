53f87a04e11c1fa784230f088a20f2f7140f0b0e
keal khondekar <kewalkhondekar25@gmail.com>
2024-09-12 12:21:31 +0530

    feat: typecasting added

--- Operators/main.cpp
+++ Operators/main.cpp
@@ -5,11 +5,16 @@ using namespace std;
 int main(){
   cout << "Operators" << endl;
   
-  cout << "Arithmatic Operators";
+  cout << "Arithmatic Operators" << endl;
   int a = 10, b = 5;
   int sum = a + b;
   int sub = a - b;
   int mul = a * b;
   int div = a/b;
-  cout << "sum: " << sum << endl << "sub: " << sub << endl << "mul: " << mul << endl << "div: " << div;
+  int mod = a % b;
+  cout << "sum: " << sum << endl << "sub: " << sub << endl << "mul: " << mul << endl << "div: " << div << endl << "mod: " << mod << endl;
+
+  cout << "division: " << (5/(double)2);
+
+
 };
