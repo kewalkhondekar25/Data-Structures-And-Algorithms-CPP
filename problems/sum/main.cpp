351a71b369f1e8b03fc5f06e82d8b82ef70ce147
keal khondekar <kewalkhondekar25@gmail.com>
2024-09-14 23:57:46 +0530

    feat: sum of 2 no. added

---
+++ problems/sum/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a, b;
+  cout << "Enter 1st Number" << endl;
+  cin >> a;
+  cout << "Enter 2nd Number" << endl;
+  cin >> b;
+  int sum = a + b;
+  cout << "sum of 2 numbers are: " << sum;
+}
