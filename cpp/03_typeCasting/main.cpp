5bc56cdd14dd332a165320068650f672d131029b
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-03 15:43:10 +0530

    feat: arithmetic, typecast added

--- cpp/03_typeCasting/main.cpp
+++ cpp/03_typeCasting/main.cpp
@@ -5,6 +5,9 @@ int main() {
 
   double price = 100.99;
   int newPrice = (int)price;
-  cout << "new price: " << newPrice;
+  cout << "new price: " << newPrice << endl;
+  
+  int a = 5, b = 3;
+  cout << "a/b: " << (a/(float)3);
   return 0;
 }
---
+++ cpp/05_operators/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  //arithmetic operators
+  int a = 2, b = 2;
+  int sum = a + b;
+  int sub = a - b;
+  int mul = a * b;
+  int div = a / b;
+  int rem = a % b;
+
+  cout << "arithmetic operators \n";
+  cout << "sum: " << sum << endl << "sub: " << sub  << endl << "mul: " << mul << endl << "div: " << div << endl << "rem: " << rem << endl;
+
+  int x = 5;
+  float y = 2;
+  int z = 3;
+  cout << "a/b: " << x/y << endl;
+
+  return 0;
+}
