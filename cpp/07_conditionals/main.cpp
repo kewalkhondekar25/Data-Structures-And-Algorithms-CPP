343327873ef0a147ff4478db318929a4fe82e633
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-06 23:41:57 +0530

    feat: conditionals added

--- cpp/07_conditionals/main.cpp
+++ cpp/07_conditionals/main.cpp
@@ -7,6 +7,21 @@ int main(){
 
   if(n >= 0){
     cout << "n is +ve \n";
+  } else{
+    cout << "n is -ve \n";
+  }
+
+  int age;
+
+  cout << "enter age: ";
+  cin >> age;
+
+  if(age < 18){
+    cout << "you can't drink \n";
+  } else if(age >= 18 && age <= 25){
+    cout << "you can drink only beer";
+  }else{
+    cout << "you can drink hard liquore";
   };
 
   return 0;
