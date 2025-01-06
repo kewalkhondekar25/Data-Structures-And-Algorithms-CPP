7130f231d236e17432fd74c318d291ff5a09430c
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-07 00:23:39 +0530

    feat: ternary added

--- cpp/08_upperOrLower/main.cpp
+++ cpp/08_upperOrLower/main.cpp
@@ -8,11 +8,18 @@ int main(){
   cout << "enter a character: ";
   cin >> alphabet;
 
+  cout << (int)alphabet << endl;//ASCII
+
   if(alphabet  >= 'a' && alphabet <= 'z'){
-    cout << "you enter lower case alphabet";
+    cout << "you enter lower case alphabet \n";
   }else{
-    cout << "you enter capital case alphabet";
+    cout << "you enter capital case alphabet \n";
   };
+
+  int num;
+  cout << "enter a number: \n";
+  cin >> num;
+  cout << (num >= 0 ? "positive" : "negative");
 
   return 0;
 };
