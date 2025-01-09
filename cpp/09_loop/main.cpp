1a022afda3539ebfd2ec687749a6377e915c8b2f
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-09 11:24:25 +0530

    feat: loops added

--- cpp/09_loop/main.cpp
+++ cpp/09_loop/main.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 int main() {
 
+  //while
+  int count = 1;
+  while( count <= 5 ){
+    cout << count << endl;
+    count++;
+  };
+
+  //for
   for(int i = 1; i <= 10; i++){
     cout << "i: " << i << endl;
   };
+
+  //sum of n
+  int sum = 0, n = 5;
+  for (int i = 1; i <= n; i++){
+    sum += i;
+  };
+  cout << "sum: " << sum;
+  
+  return 0;
 }
