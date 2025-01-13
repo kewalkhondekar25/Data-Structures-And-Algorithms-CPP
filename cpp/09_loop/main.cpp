2c46dd1600214e4eed015bdcf286c63acf823770
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-14 00:02:48 +0530

    feat: do while added

--- cpp/09_loop/main.cpp
+++ cpp/09_loop/main.cpp
@@ -18,8 +18,24 @@ int main() {
   int sum = 0, n = 5;
   for(int i = 1; i <= n; i++){
     sum += i;
-  }
-  cout << "sum: " << sum;
+  };
+  cout << "sum: " << sum << endl;
+
+  //sum of odd num 1 to 5
+  int sumOfOdd = 0;
+  for(int i = 1; i <= 5; i++){
+    if(i % 2 != 0){
+      sumOfOdd += i;
+    }
+  };
+  cout << "sum of odd: " << sumOfOdd << endl;
+
+  //do-while loop
+  int j = 1, val = 5;
+  do {
+    cout << "do while: " << j << endl;
+    j++;
+  } while( j <= val);
 
   return 0;
 }
