75903cfcff5489da080ae4235b047cd600401ac8
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-15 03:39:52 +0530

    feat: patterns added

--- cpp/09_loop/main.cpp
+++ cpp/09_loop/main.cpp
@@ -1,41 +1,70 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-  //while
+  // while
   int count = 1;
-  while( count <= 5 ){
+  while (count <= 5)
+  {
     cout << count << endl;
     count++;
   };
 
-  //for
-  for(int i = 1; i <= 10; i++){
+  // for
+  for (int i = 1; i <= 10; i++)
+  {
     cout << i << endl;
   };
 
   int sum = 0, n = 5;
-  for(int i = 1; i <= n; i++){
+  for (int i = 1; i <= n; i++)
+  {
     sum += i;
   };
   cout << "sum: " << sum << endl;
 
-  //sum of odd num 1 to 5
+  // sum of odd num 1 to 5
   int sumOfOdd = 0;
-  for(int i = 1; i <= 5; i++){
-    if(i % 2 != 0){
+  for (int i = 1; i <= 5; i++)
+  {
+    if (i % 2 != 0)
+    {
       sumOfOdd += i;
     }
   };
   cout << "sum of odd: " << sumOfOdd << endl;
 
-  //do-while loop
+  // do-while loop
   int j = 1, val = 5;
-  do {
+  do
+  {
     cout << "do while: " << j << endl;
     j++;
-  } while( j <= val);
+  } while (j <= val);
+
+  // num is prime or not;
+  int last = 7;
+  bool isPrime = true;
+
+  for (int i = 2; i <= (last - 1); i++){
+
+    if (last % i == 0){
+      isPrime = false;
+      break;
+    };
+
+  };
+
+  if(isPrime == true){
+    cout << "prime \n";
+  }
+  else{
+    cout << "non prime \n";
+  };
+
+  //nested loop
 
   return 0;
 }
