9a0cf33c08820a7e5bfd50f7ebc7d4d3804a02cf
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-15 03:47:09 +0530

    feat: stars patterns added

--- cpp/10_pattern/02_starPattern/main.cpp
+++ cpp/10_pattern/02_starPattern/main.cpp
@@ -5,9 +5,9 @@ int main(){
   
   int n = 4;
 
-  for(int i = 1; i <= n; i++){
+  for(int i = 0; i < n; i++){
 
-    for(int j = 1; j <= n; j++){
+    for(int j = 0; j < n; j++){
       cout << "*" << " ";
     };
 
