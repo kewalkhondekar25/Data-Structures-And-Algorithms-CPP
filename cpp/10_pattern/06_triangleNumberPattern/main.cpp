24ab36073ecef05cf8196f3ed080ce4b8eed4e71
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-21 19:38:25 +0530

    feat: triangle pattern added

--- cpp/10_pattern/06_triangleNumberPattern/main.cpp
+++ cpp/10_pattern/06_triangleNumberPattern/main.cpp
@@ -12,15 +12,30 @@ int main() {
 
     cout << endl;
   };
+
+  for(int i = 0; i < n; i++){
+
+    for(int j = 1; j <= (i + 1); j++){
+      cout << j;
+    };
+    
+    cout << endl;
+  };
+
   
   return 0;
 }
 
 /*
-
+OUTPUT - 1
 1
 2 2
 3 3 3
 4 4 4 4
 
+OUTPUT - 2
+1
+1 2
+1 2 3
+1 2 3 4
 */
