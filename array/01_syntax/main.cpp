9e97f8341dcaa0f6420e22e8fccd86f64608fd90
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-23 04:36:45 +0530

    feat: arrays added

---
+++ array/01_syntax/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int marks [5] = { 10, 11, 12, 13, 14};
+
+  cout << marks[0] << endl;
+  cout << marks[1] << endl;
+  cout << marks[2] << endl;
+  cout << marks[3] << endl;
+  cout << marks[4] << endl;
+
+  return 0;
+};
