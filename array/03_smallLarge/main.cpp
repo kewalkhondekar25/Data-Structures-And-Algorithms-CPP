d39c80a643c6c66a92e7fb42d7c2dcc0c4c582ef
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-23 05:25:51 +0530

    feat: smallest and largest added

--- array/03_smallLarge/main.cpp
+++ array/03_smallLarge/main.cpp
@@ -9,13 +9,29 @@ int main() {
   int size = 5;
   int smallest = INT_MAX;
 
+  //smallest in an array
   for(int i = 0; i < size; i++){
+
     if(arr[i] < smallest){
       smallest = arr[i];
     };
+
   };
 
-  cout << "smallest is: " << smallest;
+  cout << "smallest is: " << smallest << endl;
+
+  //largest in an array
+  int largest = INT_MIN;
+  for(int i = 0; i < size; i++){
+
+    if(arr[i] > largest){
+      largest = arr[i];
+    };
+
+  };
+
+  cout << "largest is: " << largest;
+
 
   return 0;
 }
