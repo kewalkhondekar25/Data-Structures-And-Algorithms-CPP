7f6acd3ade58dc73311c8b853c6ee8fe7ecfe7fe
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-26 18:35:43 +0530

    feat: STL - array pairs added

--- cpp/14_STL/01_pairs/main.cpp
+++ cpp/14_STL/01_pairs/main.cpp
@@ -7,7 +7,12 @@ int main() {
   cout << p1.first << " " << p1.second << endl;
 
   pair<int, pair<int, int>> p2 = {1, {1, 69}};
-  cout << p2.first << " " << p2.second.first << " " << p2.second.second;
+  cout << p2.first << " " << p2.second.first << " " << p2.second.second << endl;
+
+  pair<int, int> arr[] = { {1, 2}, {3, 4}, {5, 6}};
+  cout << arr[0].first << " " << arr[0].second << endl;
+  cout << arr[1].first << " " << arr[1].second << endl;
+  cout << arr[2].first << " " << arr[2].second << endl;
 
   return 0;
 }
