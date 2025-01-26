a924d0a8b54390313cffc33fb32543ca20982c20
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-26 18:31:06 +0530

    feat: STL - multiple pairs added

--- cpp/14_STL/01_pairs/main.cpp
+++ cpp/14_STL/01_pairs/main.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main() {
 
-  pair<int, int> p = {1, 69};
-  
-  cout << p.first << " " << p.second;
+  pair<int, int> p1 = {1, 69};
+  cout << p1.first << " " << p1.second << endl;
+
+  pair<int, pair<int, int>> p2 = {1, {1, 69}};
+  cout << p2.first << " " << p2.second.first << " " << p2.second.second;
 
   return 0;
 }
