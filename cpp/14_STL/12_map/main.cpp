18cd87fe1be19b37299d7d1a7a4514a0dd7e7f37
keal khondekar <kewalkhondekar25@gmail.com>
2025-01-27 21:47:03 +0530

    feat: map added

---
+++ cpp/14_STL/12_map/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  //unique keys in sorted
+  map<int, int> mp;
+
+  //INSERT
+  mp[1] = 69;
+  mp.emplace(3, 71);
+  mp.emplace(2, 70);
+
+  for(auto val : mp){
+    cout << val.first << ": " << val.second << endl;  // { 1: 69, 2: 70, 3: 71 }
+  };
+
+  cout << mp[1] << endl;  //69
+  cout << mp[4] << endl;  //0
+
+  auto it = mp.find(1);
+  // cout << *(it) << endl;
+
+  return 0;
+};
