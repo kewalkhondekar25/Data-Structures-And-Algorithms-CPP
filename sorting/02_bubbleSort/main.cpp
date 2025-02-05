af894e88187a3c9bb2b5ba0b59c6305b59ac9e86
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-05 23:59:30 +0530

    feat: selection sort added

---
+++ sorting/02_bubbleSort/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n - 1 + i; j++){
+      if(arr[j] > arr[j+1]){
+        swap(arr[j], arr[j+1]);
+      }
+    }
+  }
+}
+
+int main(){
+
+  int arr[6] = {13, 46, 24, 52, 20, 9};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  
+  bubble_sort(arr, n);
+
+  for(int val : arr){
+    cout << val << endl;
+  };
+
+  return 0;
+}
