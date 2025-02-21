277552a8db7f1e52e95dc71e99e02b3e66908141
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-21 16:54:14 +0530

    feat: binary search added

---
+++ binarySearch/01_findTargetByIterative/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary_search(vector<int> &arr, int size, int target){
+
+  int low = arr[0];
+  int high = size - 1;
+
+  while(low <= high){
+    int mid = low + high / 2;
+    if(arr[mid] == target){
+      return mid;
+    }else if(target > arr[mid]){
+      low = mid + 1;
+    }else{
+      high = mid - 1;
+    };
+  };
+  return -1;
+}
+
+int main(){
+
+  vector<int> arr = { 3, 4, 6, 7, 9, 12, 16, 17 };
+  cout << binary_search(arr, arr.size(), 16);
+
+  return 0;
+}
