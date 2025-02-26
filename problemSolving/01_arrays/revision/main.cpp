b147ec59989628e7f4d4125fb651acc9ef5b7010
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-26 13:57:36 +0530

    feat: second largest added

--- problemSolving/01_arrays/revision/main.cpp
+++ problemSolving/01_arrays/revision/main.cpp
@@ -3,23 +3,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int find_largest(vector<int> &arr, int n){
+int find_second_largest(vector<int> &arr, int n){
 
   int largest = arr[0];
+  int slargest = -1;
 
   for(int i = 0; i < n; i++){
+    
     if(arr[i] > largest){
+      slargest = largest;
       largest = arr[i];
+    }else if(arr[i] < largest && arr[i] > slargest){
+      slargest = arr[i];
     };
   };
-  return largest;
+
+  return slargest;
 };
 
 int main(){
 
-  vector<int> arr = { 1, 3, 2, 5, 4};
+  vector<int> arr = { 1, 2, 3, 7, 7, 5};
 
-  cout << "largest: " << find_largest(arr, arr.size());
+  cout << "largest: " << find_second_largest(arr, arr.size());
 
   return 0;
 }
