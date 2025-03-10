f89bf3bd412d8c7b3d27fdcf46ac4572b81aceb5
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-03-10 14:00:08 +0530

    feat: 5*5 patterns added

--- logicBuilding/01_patterns/01/main.cpp
+++ logicBuilding/01_patterns/01/main.cpp
@@ -1,8 +1,26 @@
 #include <bits/stdc++.h>
 using  namespace std;
 
+void print(int n){
+
+  for(int i = 0; i < n; i++){
+
+    for(int j = 0; j < n; j++){
+      cout << "* ";
+    };
+
+    cout << endl;
+  };
+};
+
 int main(){
 
-  cout << "patterns";
+  int n;
+
+  cout << "Enter n: ";
+  cin >> n;
+  
+  print(n);
+
   return 0;
 }
