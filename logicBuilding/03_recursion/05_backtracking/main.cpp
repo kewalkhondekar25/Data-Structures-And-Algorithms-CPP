3722f58a0aed86ceb7b99d5112caef850979e3e1
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-03-21 20:30:40 +0530

    feat: recursion backtracking added

---
+++ logicBuilding/03_recursion/05_backtracking/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printNum(int i, int n){
+
+  if(i < 1) return;
+
+  printNum(i - 1, n);
+
+  cout << i << endl;
+};
+
+int main(){
+  int n;
+  cout << "Enter n: ";
+  cin >> n;
+
+  printNum(n, n);
+  
+  return 0;
+}
