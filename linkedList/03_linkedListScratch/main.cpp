61cb462fd2370dbca8c60165e6ea48979f91312b
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-03-29 00:10:37 +0530

    feat: linkedlist implementation added

--- linkedList/03_linkedListScratch/main.cpp
+++ linkedList/03_linkedListScratch/main.cpp
@@ -27,13 +27,31 @@ class List {
       if(head == NULL){
         head = tail = newNode;
         return;
+      }else{
+        newNode->next = head;
+        head = newNode;
       };
+    };
+
+    void printLL(){
+      Node* temp = head;
+      while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+      };
+      cout << endl;
     };
 };
 
 int main(){
   
   List ll;
+
+  ll.push_front(1);
+  ll.push_front(2);
+  ll.push_front(3);
+
+  ll.printLL();
   
   return 0;
 }
