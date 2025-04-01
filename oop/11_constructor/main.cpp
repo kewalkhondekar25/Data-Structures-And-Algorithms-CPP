f7a3b22f884b4ba347234781d61198f8d06b45e1
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-04-02 01:29:27 +0530

    feat: constructor overloading added

--- oop/11_constructor/main.cpp
+++ oop/11_constructor/main.cpp
@@ -5,13 +5,32 @@ class Test {
 
   int a, b;
   public:
-    Test(){
+    Test(){//default constructor
       a = 10;
       b = 20;
     };
 
     void get(){
-      cout << a << endl << b;
+      cout << a << endl << b << endl;
+    };
+};
+
+class Circle {
+
+  float r;
+
+  public:
+    //constructor overloading
+    Circle(){
+      r = 2;
+    };
+    
+    Circle(float r){//parameterized constructor
+      this->r = r;
+    };
+
+    void area(){
+      cout << 3.14 * r * r << endl;
     };
 };
 
@@ -19,6 +38,11 @@ int main(){
 
   Test t; 
   t.get();
-  
+
+  Circle c1;
+  Circle c2(4);
+  c1.area();
+  c2.area();
+
   return 0;
 }
