4f18d1ac107e52386e13faf0816074d55c010f59
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-04-18 23:47:57 +0530

    feat: left rotate by n places added

---
+++ revision/01_array/easy/06_leftRotateByNplaces/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void leftRotateByNplaces(vector<int> &arr, int n){
+
+  
+}
+
+int main(){
+
+  vector<int> arr = { 1, 2, 3, 4, 5 };//2, 3, 4, 5, 1
+  int n = arr.size();
+
+  leftRotateByNplaces(arr, n);
+
+  for(int val : arr){
+    cout << val << endl;
+  };
+
+  return 0;
+};
