a12f2bdb80bc2bfa858f1e78548c525d1012ad58
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-05-05 23:25:22 +0530

    feat: linked list added

---
+++ linkedList/04_basic/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+  
+  public:
+    int data;
+    Node* next;
+
+    Node(int val){
+      data = val;
+      next = NULL;
+    };
+};
+
+class List{
+
+  Node* head;
+  Node* tail;
+
+  public:
+    List(){
+      head = tail = NULL;
+    };
+
+    void push_front(int val){
+      Node* newNode = new Node(val);
+      if(head == NULL){
+        head = tail = newNode;
+      }else{
+        newNode->next = head;
+        head = newNode;
+      }
+    };
+
+    void printLinkedList(){
+      Node* temp = head;
+      while(temp != NULL){
+        cout << temp->data << endl;
+        temp = temp->next;
+      };
+    };
+};
+
+int main(){
+  
+  List ll;
+  
+  ll.push_front(1);
+  ll.push_front(2);
+  ll.push_front(3);
+
+  ll.printLinkedList();
+
+  return 0;
+};
