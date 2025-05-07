02b7b68c5af26818960fa021ba65296888882cee
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-05-07 23:54:56 +0530

    feat: linkedlist pop front added

--- linkedList/04_basic/main.cpp
+++ linkedList/04_basic/main.cpp
@@ -43,6 +43,16 @@ class List{
       }
     };
 
+    void pop_front(){
+      if(head == NULL){
+        return;
+      };
+      Node* temp = head;
+      head = head->next;
+      temp->next = NULL;
+      delete temp;
+    };
+
     void printLinkedList(){
       Node* temp = head;
       while(temp != NULL){
@@ -61,6 +71,10 @@ int main(){
   ll.push_front(3);
   ll.push_back(69);
 
+  ll.printLinkedList();
+
+  cout << "pop front" << endl;
+  ll.pop_front();
   ll.printLinkedList();
 
   return 0;
