e1ef2db0f265fd4be2ecd8c5ec1567a8eb33572a
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-05-25 22:07:59 +0530

    fet: add hashing for apperance one

--- problemSolving/01_arrays/01_easy/13_findThatAppearOnce/main.cpp
+++ problemSolving/01_arrays/01_easy/13_findThatAppearOnce/main.cpp
@@ -3,27 +3,52 @@
 
 using namespace std;
 
-int findThatAppearOnce(vector<int> &arr, int n){
+/*BRUTE*/
+// int findThatAppearOnce(vector<int> &arr, int n){
+//   for(int i = 0; i < n; i++){//O(n^2)
+
+//     int flag = 0;
+    
+//     for(int j = 0; j< n; j++){
+//       if(arr[i] == arr[j]){
+//         flag += 1;
+//       }
+//     };
+//     if(flag == 1){
+//       return arr[i];
+//     };
+//   }
+//   return -1;
+// };
+
+/*BETTER*/
+int findThatAppearOnce(vector<int> &arr, int n){// TC => O(3n); SC => O(max elem)
+
+  int max = arr[0];
+  for(int i = 0; i < n; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    };
+  };
+
+  int hash[max + 1] = { 0 };
 
   for(int i = 0; i < n; i++){
-    
-    int flag = 0;
-    
-    for(int j = 0; j < n; j++){
-      if(arr[i] == arr[j]){
-        flag += 1;
-      }
-    };
+    hash[arr[i]] += 1;
+  };
 
-    if(flag == 0){
-      return arr[i];
+  for(int j = 0; j < n; j++){
+    if(hash[j] == 1){
+      return j;
     };
   };
 
   return -1;
 }
+
+
 int main(){
-  vector<int> arr = { 1, 1, 2, 2, 3 };
+  vector<int> arr = { 4, 1, 2, 1, 2 };
   int n = arr.size();
 
   cout << findThatAppearOnce(arr, n);
